2731a1ffc4f2c3856bfd688d778e561adab6adf5
System Administrator <root@xingobarde-MacBook-Pro.local>
2017-04-08 18:27:27 +0800

    sidenav animation

---
+++ side navigation/js/sidenav.js
@@ -0,0 +1,43 @@
+
+// sidenav overlay 
+// function openNav(){
+// 	$("#sideNavigation").css('width','250px');
+// }
+// function closeNav(){
+// 	$("#sideNavigation").css('width','0px');
+// }
+
+
+// sidenav push
+// function openNav(){
+// 	$("#sideNavigation").css('width','250px');
+// 	$("#main").css('margin-left','250px');
+// }
+// function closeNav(){
+// 	$("#sideNavigation").css('width','0px');
+// 	$("#main").css('margin-left','0px');
+// }
+
+// push content opacity
+// function openNav(){
+// 	$("#sideNavigation").css('width','250px');
+// 	$("#main").css('margin-left','250px');
+// 	$('body').css('background','rgba(0,0,0,0.4)');
+// }
+// function closeNav(){
+// 	$("#sideNavigation").css('width','0px');
+// 	$("#main").css('margin-left','0px');
+// 	$('body').css('background','white');
+// }
+
+// sidenav full width
+function openNav(){
+	$("#sideNavigation").css('width','100%');
+}
+function closeNav(){
+	$("#sideNavigation").css('width','0%');
+}
+
+
+
+
