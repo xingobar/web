4f44f2e6f1f83641765114746db8bffd9000eff3
System Administrator <root@xingobarde-MacBook-Pro.local>
2017-04-08 17:55:14 +0800

    sidebar animation

--- sidebar/js/sidebar.js
+++ sidebar/js/sidebar.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-	$(".main-menu").animate({
-		left:'0px',
-		top:'0px',
-		bottom:'0px',
-		height:'100%',
-		width:'52px',
-	},1000);
-});
+// $(document).ready(function(){
+// 	$(".main-menu").animate({
+// 		left:'0px',
+// 		top:'0px',
+// 		bottom:'0px',
+// 		height:'100%',
+// 		width:'52px',
+// 		transition:'width 1.65s  cubic-bezier(0.680, -0.550, 0.265, 1.550)',
+// 	},1000);
+// });
